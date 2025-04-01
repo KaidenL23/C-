d66fef1481e0689056241bdaab20a383ffa61360
KaidenL23 <kaiden.j.ketcher04@gmail.com>
2025-04-01 16:15:22 -0500

    Add files via upload
    Week 8 to 10 Lab code

---
+++ TextAnalysis.cs
@@ -0,0 +1,17 @@
+namespace TextAnalysis_Project
+{
+    public partial class TextAnalysisForm : Form
+    {
+        public TextAnalysisForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TextAnalysisForm_Load(object sender, EventArgs e)
+        {
+
+        
+
+        }
+    }
+}
