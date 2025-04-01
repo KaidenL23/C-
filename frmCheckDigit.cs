46e452146e8f536f2cb7a9fbf0348d1b5c7c55b6
KaidenL23 <kaiden.j.ketcher04@gmail.com>
2025-04-01 16:12:11 -0500

    Add files via upload
    Week 9 Code

---
+++ frmCheckDigit.cs
@@ -0,0 +1,35 @@
+namespace Check_Digit_Project
+{
+    public partial class frmCheckDigit : Form
+    {
+        public frmCheckDigit()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblProcessingInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
